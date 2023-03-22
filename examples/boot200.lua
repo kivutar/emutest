3f0829bb8ff2a42f231b64a20ca9f6307cadae0b
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-03-22 16:46:29 +0700

    Update tests

--- examples/boot200.lua
+++ examples/boot200.lua
@@ -1,7 +1,8 @@
+set_options_string("duckstation_HLE___Enable = \"true\"\nduckstation_CPU___ExecutionMode = \"Interpreter\"")
 load_core(corepath)
 load_game(rompath)
 
-for i=1,200 do
+for i=1,2000 do
 	run()
 end
 
